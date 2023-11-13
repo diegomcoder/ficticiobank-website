d42fafde0214b00c77aa9ad34f5decd840f5fd0e
diegomcoder <mdsidiego@gmail.com>
2023-11-13 14:29:57 -0300

    improve website layout

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-const ACCOUNT_DB = [];
+// const ACCOUNT_DB = [];
 
-const BUTTONS = {
-	'openAccount': document.querySelector(".open-account-btns")
-}
+// const BUTTONS = {
+// 	'openAccount': document.querySelector(".open-account-btn")
+// }
 
-BUTTONS.openAccount.addEventListener('click', function() {
-	window.open('../new-account.html', '_blank');
-})
+// BUTTONS.openAccount.addEventListener('click', function() {
+// 	window.open('../new-account.html', '_blank');
+// })
 
-alert('funciona')
+// alert('funciona')
+
+const myNumberInput = document.getElementById("withdrawValue");
+
+myNumberInput.addEventListener("change", () => {
+  myNumberInput.value = parseFloat(myNumberInput.value).toFixed(2);
+});
